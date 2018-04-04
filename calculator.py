6db1e97e7da606a776ca056b10eb9f6b0c72a9bc
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-04 16:33:21 -0700

    Calculator program works for one input by user

--- calculator.py
+++ calculator.py
@@ -7,13 +7,40 @@ calculator program yourself in this file.
 from arithmetic import *
 
 
-# Your code goes here
 def calculator():
     print 'To use the caluculator enter the operation in the format: \"operator num1 num2\"'
     user_input = raw_input('>')
+    if user_input == 'q' or user_input == 'Q':
+        return
     try:
         input_list = user_input.split(" ")
+        operator = input_list[0]
+        nums = list(map(lambda x: int(x), input_list[1:]))
+        print "Type of nums: ", type(nums[0])
         print "input list: ", input_list
+        num1 = float(nums[0])
+        num2 = float(nums[1]) or None    
+        
+        if operator == '+':
+            print add(num1, num2)
+        elif operator == '-':
+            print subtract(num1, num2)
+        elif operator == '*':
+            print multiply(num1, num2)
+        elif operator == '/':
+            print divide(num1, num2)
+        elif operator == 'square':
+            print square(num1)
+        elif operator == 'cube':
+            print cube(num1)
+        elif operator == 'power':
+            print power(num1, num2)
+        elif operator == 'mod':
+            print mod(num1, num2)
+        else:
+            print "Hmmm, I didn\'t catch that. Please try again using the valid format: \"operator num1 num2\""
+            calculator()
+
     except:
         print "Hmmm, I didn\'t catch that. Please try again using the valid format: \"operator num1 num2\""
         calculator()
