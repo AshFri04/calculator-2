b79d689d0c5cb16922e6dd604ba9d4e2f9dce2ba
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-04 17:34:53 -0700

    All helper functions completed! Working program.

--- calculator.py
+++ calculator.py
@@ -10,35 +10,9 @@ def calculator():
     user_input = raw_input('>')
     if quitting(user_input):
         return
-    try:
-        input_list = user_input.split(" ")
-        operator = input_list[0]
-        nums = list(map(lambda x: int(x), input_list[1:]))
-        print "Type of nums: ", type(nums[0])
-        print "input list: ", input_list
-        num1 = float(nums[0])
-        num2 = float(nums[1]) or None    
-        
-        if operator == '+':
-            print add(num1, num2)
-        elif operator == '-':
-            print subtract(num1, num2)
-        elif operator == '*':
-            print multiply(num1, num2)
-        elif operator == '/':
-            print divide(num1, num2)
-        elif operator == 'square' and num2 == None:
-            print square(num1)
-        elif operator == 'cube' and num2 == None:
-            print cube(num1)
-        elif operator == 'power':
-            print power(num1, num2)
-        elif operator == 'mod':
-            print mod(num1, num2)
-        else:
-            print "Hmmm, I didn\'t catch that. Please try again using the valid format: \"operator num1 num2\""
-        calculator()
-
+    try: 
+        num1, num2, operator = get_nums(user_input)
+        do_calculation(num1, num2, operator) 
     except:
         throw_exception()
 
@@ -53,5 +27,34 @@ def throw_exception():
 
 def quitting(user_input):
     return (user_input == 'q') or (user_input == 'Q')
+
+def get_nums(user_input):
+    input_list = user_input.split(" ")
+    operator = input_list[0]
+    nums = list(map(lambda x: int(x), input_list[1:]))
+    num1 = float(nums[0])
+    num2 = float(nums[1]) or None
+    return num1, num2, operator   
+
+def do_calculation(num1, num2, operator):
+    if operator == '+':
+        print add(num1, num2)
+    elif operator == '-':
+        print subtract(num1, num2)
+    elif operator == '*':
+        print multiply(num1, num2)
+    elif operator == '/':
+        print divide(num1, num2)
+    elif operator == 'square' and num2 == None:
+        print square(num1)
+    elif operator == 'cube' and num2 == None:
+        print cube(num1)
+    elif operator == 'power':
+        print power(num1, num2)
+    elif operator == 'mod':
+        print mod(num1, num2)
+    else:
+        print "Hmmm, I didn\'t catch that. Please try again using the valid format: \"operator num1 num2\""
+    calculator()
 
 init()
